1ca4aee3262e50a30467ef59cf5aa572513d254a
Danilo Noguera <danilo.noguera@gmail.com>
2023-08-25 13:48:00 -0600

    Calculo de salario neto - Variables creadas

---
+++ CalculoSalarioNeto/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    static final double tasaINSS = 7.0;
+    static final double tasaIR = 15.6;
+    static final double tasaINSSPatronal = 22.5;
+    static final double tasaSindicato = 1.0;
+    public static void main(String[] args) {
+        //Declaracion de variables
+        double salarioBruto, ingresoNeto, ingresoTotal;
+        double ingresoAntiguedad, deduccionesTotales;
+        double deduccionINSS, deduccionIR, deduccionSindicato;
+        double deduccionINSSPatronal;
+    }
+}
